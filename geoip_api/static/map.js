de29a22eb80d9a61f75e4a840fae4737c37b5d1f
David Sn <divad.nnamtdeis@gmail.com>
2022-04-06 17:05:02 +0200

    Add feature to lookup IP addresses on landing page
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- geoip_api/static/map.js
+++ geoip_api/static/map.js
@@ -1,4 +1,7 @@
+'use strict';
+
 var map = L.map('map').setView([0, 0], 1);
+var markerGroup = new L.FeatureGroup().addTo(map);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,21 +13,44 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiY29kZWJ1Y2tldCIsImEiOiJjajF0cGVkcmswMDMwMzJyemIxYmxncnpiIn0.Y6Cnw8QVXgLXAPdsc2E0Nw'
 }).addTo(map);
 
+document.getElementById('lookup').onsubmit = function(e) {
+    e.preventDefault();
+
+    let ipAddress = document.getElementById('ip').value;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '/api/' + ipAddress, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        // Check if we have a valid response
+        if (xhr.status >= 200 && xhr.status < 400) {
+            geoip(xhr.response);
+        } else {
+            alert('Invalid public IPv4 or IPv6 Address');
+        }
+    };
+    xhr.send();
+
+    // Reset form after submit
+    e.target.reset();
+};
+
 function geoip(response) {
     document.getElementById('ip_address').innerText = response.ip_address;
     document.getElementById('hostname').innerText = response.hostname || response.ip_address;
-    document.getElementById('country').innerHTML = response.country.name + ' <span class="flag-icon flag-icon-' + response.country.iso_code.toLowerCase() + '"></span>';
+    document.getElementById('country').innerHTML = response.country.name + '&ensp;<span class="flag-icon flag-icon-' + response.country.iso_code.toLowerCase() + '"></span>';
     document.getElementById('city').innerText = response.city.name || 'Not available';
     document.getElementById('asn').innerText = 'AS' + response.asn.id + ' - ' + response.asn.name;
 
+    // Remove marker before creating new
+    markerGroup.clearLayers();
+
     // Add location and accuracy radius
-    let marker = L.marker([response.location.latitude, response.location.longitude]);
-    let accuracy_circle = L.circle([response.location.latitude, response.location.longitude], response.location.accuracy_radius * 1000);
+    L.marker([response.location.latitude, response.location.longitude]).addTo(markerGroup);
+    L.circle([response.location.latitude, response.location.longitude], response.location.accuracy_radius * 1000).addTo(markerGroup);
 
     // Center view to marker group
-    let group = L.featureGroup([marker, accuracy_circle]).addTo(map);
-
-    map.fitBounds(group.getBounds(), {
+    map.fitBounds(markerGroup.getBounds(), {
         padding: [50, 50]
     });
-};
+}
