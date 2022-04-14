bdc0c1e969f6ed589ab1bb1a163146b9dfacc1f9
David Sn <divad.nnamtdeis@gmail.com>
2022-04-14 12:41:09 +0200

    Move appplication init to seperate function
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- geoip_api/__init__.py
+++ geoip_api/__init__.py
@@ -11,60 +11,62 @@ from geoip_api.routes import router
 
 BASE_DIR = Path(__file__).resolve().parent
 
-app = FastAPI(
-    debug=False,
-    title="GeoIP API",
-    description="A restful GeoIP API powered by MaxMind GeoLite2.",
-    version="1.0",
-    license_info={
-        "name": "MIT License",
-        "url": "https://github.com/divadsn/geoip-api/blob/master/LICENSE",
-    },
-    docs_url="/docs/",
-    redoc_url=None,
-    openapi_url="/api/openapi.json",
-)
 
-app.include_router(router)
-app.mount("/", StaticFiles(directory=Path(BASE_DIR, "static"), html=True))
+def get_application() -> FastAPI:
+    app = FastAPI(
+        debug=False,
+        title="GeoIP API",
+        description="A restful GeoIP API powered by MaxMind GeoLite2.",
+        version="1.0",
+        license_info={
+            "name": "MIT License",
+            "url": "https://github.com/divadsn/geoip-api/blob/master/LICENSE",
+        },
+        docs_url="/docs/",
+        redoc_url=None,
+        openapi_url="/api/openapi.json",
+    )
 
-redis = aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+    redis = aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+
+    async def limiter_identifier(request: Request):
+        forwarded = request.headers.get("X-Forwarded-For")
+
+        if forwarded:
+            ip_address = forwarded.split(",")[0]
+        else:
+            ip_address = request.client.host
+
+        return ip_address
+
+    @app.middleware("http")
+    async def add_cache_control_header(request: Request, call_next):
+        response = await call_next(request)
+
+        if request.url.path.startswith("/api/"):
+            response.headers["Cache-Control"] = "no-cache"
+
+        return response
+
+    @app.on_event("startup")
+    async def startup_event():
+        await FastAPILimiter.init(redis, identifier=limiter_identifier)
+
+    @app.on_event("shutdown")
+    async def shutdown_event():
+        await redis.close()
+
+    @app.get("/healthcheck", include_in_schema=False)
+    async def healthcheck():
+        return {
+            "success": True,
+            "message": "healthy",
+        }
+
+    app.include_router(router)
+    app.mount("/", StaticFiles(directory=Path(BASE_DIR, "static"), html=True))
+
+    return app
 
 
-async def limiter_identifier(request: Request):
-    forwarded = request.headers.get("X-Forwarded-For")
-
-    if forwarded:
-        ip_address = forwarded.split(",")[0]
-    else:
-        ip_address = request.client.host
-
-    return ip_address
-
-
-@app.middleware("http")
-async def add_cache_control_header(request: Request, call_next):
-    response = await call_next(request)
-
-    if request.url.path.startswith("/api/"):
-        response.headers["Cache-Control"] = "no-cache"
-
-    return response
-
-
-@app.on_event("startup")
-async def startup_event():
-    await FastAPILimiter.init(redis, identifier=limiter_identifier)
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await redis.close()
-
-
-@app.get("/healthcheck", include_in_schema=False)
-async def healthcheck():
-    return {
-        "success": True,
-        "message": "healthy",
-    }
+app = get_application()
